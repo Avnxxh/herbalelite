271cf4975feddb8a9a671dd495bf9bf94504c3fb
Avnxxh <pramanavi88@gmail.com>
2025-09-09 23:10:55 +0530

    new bug solve

--- app/admin/plants/page.tsx
+++ app/admin/plants/page.tsx
@@ -1,6 +1,6 @@
 // app/admin/plants/page.tsx
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
